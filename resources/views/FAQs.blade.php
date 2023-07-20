1933a644c9a5bda0d4812ec5ba0d0b07d797e74d
Muhammad Uzair Nisar <muhammaduzairnisar01@gmail.com>
2023-07-20 23:10:55 +0500

    beautification in FAQS page

--- resources/views/FAQs.blade.php
+++ resources/views/FAQs.blade.php
@@ -1,121 +1,159 @@
-@include('bootStrap')
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;900&display=swap");
+
+:root {
+	--primary: #191919;
+	--secondary: #f26db6;
+	--ternary: #310273;
+	--background: #f1f1f1;
+	--gray: #e1eeff7f;
+	--white: #fff;
+}
+
 body {
-  font-family: "IBM Plex Sans", sans-serif;
-  background-color: rgba(0,0,0,.1);
+	color: var(--font-color);
+	font-family: "Inter", sans-serif;
+	font-size: 1.2em;
+	line-height: 1.6;
+	background: var(--background);
+	overflow-x: hidden;
+	min-height: 100vh;
+	min-width: 100vw;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 
-h2 {
-  margin: 20px auto 80px;
-  font-size: 38px;
-  font-weight: 300;
-  text-align: center;
-  letter-spacing: 2px;
-  line-height: 1.5;
+.caption {
+	font-size: 10px;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 13px;
+	letter-spacing: 0.04em;
+	text-align: left;
+	text-transform: uppercase;
+	color: var(--secondary);
+}
+.title {
+	font-size: 24px;
+	font-style: normal;
+	font-weight: bold;
+	line-height: 31px;
+	letter-spacing: 0em;
+	text-align: center;
+	margin-bottom: 8px;
+	color: var(--secondary);
 }
 
-details {
-  width: 75%;
-  min-height: 5px;
-  max-width: 700px;
-  padding: 45px 70px 45px 45px;
-  margin: 0 auto;
-  position: relative;
-  font-size: 22px;
-  border: 1px solid rgba(0,0,0,.1);
-  border-radius: 15px;
-  box-sizing: border-box;
-  transition: all .3s;
+.faq {
+	max-width: 800px;
+	margin: auto;
+}
+.box-content-colapse {
+	width: 70%;
+	height: auto;
+	position: relative;
 }
 
-details + details {
-  margin-top: 20px;
+.intro-colapse {
+	width: 100%;
+	height: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 
-details[open] {
-  min-height: 50px;
-  background-color: #f6f7f8;
-  box-shadow: 2px 2px 20px rgba(0,0,0,.2);
+.details-comp {
+	cursor: pointer;
+	width: calc(100% - 32px);
+	padding: 16px;
+	border-radius: 8px;
+	margin-bottom: 32px;
+	position: relative;
+	left: 0px;
+	background: white;
+	filter: drop-shadow(7px 9px 5px rgba(0, 0, 0, 0.04));
+	-webkit-filter: drop-shadow(7px 9px 5px rgba(0, 0, 0, 0.04));
+	-moz-filter: drop-shadow(7px 9px 5px rgba(0, 0, 0, 0.04));
 }
 
-details p {
-  color: #96999d;
-  font-weight: 300;
+.details-comp > p {
+	padding: 0 16px;
 }
 
-summary {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-weight: 500;
-  cursor: pointer;
+.summary-colapse {
+	list-style: none;
+	border-radius: 8px;
+	padding-left: 16px;
+	color: var(--ternary);
+	font-weight: bold;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+}
+.summary-colapse::-webkit-details-marker {
+	display: none;
 }
 
-summary:focus {
-  outline: none;
-  
+.details-comp[open] > .summary-colapse::after {
+	display: flex;
+	justify-content: space-between;
+	content: url("https://cdn-icons-png.flaticon.com/24/7153/7153564.png");
+	font-size: 16px;
+	align-items: center;
+	padding-right: 24px;
 }
 
-summary:focus::after {
-  content: "";
-  height: 100%;
-  width: 100%;
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  box-shadow: 0 0 0 5px rebeccapurple;
+.details-comp > .summary-colapse::after {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	content: url("https://cdn-icons-png.flaticon.com/24/7153/7153566.png");
+	font-size: 16px;
+	padding-right: 24px;
 }
 
-summary::-webkit-details-marker {
-  display: none
+.details-comp[open] p {
+	animation: details-show 350ms linear;
 }
 
-.control-icon {
-  fill: rebeccapurple;
-  transition: .3s ease;
-  pointer-events: none;
+.details-comp:not([open]) p {
+	opacity: 0;
 }
 
-.control-icon-close {
-  display: none;
+@keyframes details-show {
+	0% {
+		opacity: 0;
+		transform: translatey(-25px);
+	}
+	100% {
+		opacity: 1;
+		transform: translatey(0);
+	}
 }
 
-details[open] .control-icon-close {
-  display: initial;
-  transition: .3s ease;
-}
-
-details[open] .control-icon-expand {
-  display: none;
-}
 </style>
-<h2>Frequently Asked Questions</h2>
-
-<div style="visibility: hidden; position: absolute; width: 0px; height: 0px;">
-  <svg xmlns="http://www.w3.org/2000/svg">
-    <symbol viewBox="0 0 24 24" id="expand-more">
-      <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/>
-    </symbol>
-    <symbol viewBox="0 0 24 24" id="close">
-      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/>
-    </symbol>
-  </svg>
-</div>
 
 
-  
-@foreach ($data as $item)
-<details>
-  <summary>
+<div class="box-content-colapse">
+	<div class="intro-colapse">
+		<span class="caption primary-medium-color">check our faq</span>
+		<h4 class="title">
+			Still have a question?
+		</h4>
+	</div>
+  @foreach ($data as $item)
+
+	<details class="details-comp">
+		<summary class="summary-colapse">
     {{ $item->question }}
-    <svg class="control-icon control-icon-expand" width="24" height="24" role="presentation">
-      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#expand-more" />
-    </svg>
-    <svg class="control-icon control-icon-close" width="24" height="24" role="presentation">
-      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#close" />
-    </svg>
-  </summary>
-  <p>{{ $item->answer }}</p>
-</details>
-@endforeach
+		</summary>
+		<p> {{ $item->answer }}</p>
+	</details>
+  @endforeach
+
+</div>
